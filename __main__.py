66b41312e1f0e54ab8cbebae98015fc4775e9b04
Lexo Alonzo <lexo@MacBook-Pro-de-Lexo.local>
2017-09-09 00:49:50 -0600

    first commit

---
+++ __main__.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python3
+
+import speech_recognition as sr
+from gtts import gTTS
+import json
+import apiai
+from playsound import playsound
+
+
+import arduino
+
+CLIENT_ACCESS_TOKEN = '0be9930a8de146deb3014346c5de1d9d'
+ai = apiai.ApiAI(CLIENT_ACCESS_TOKEN)
+request = ai.text_request()
+request.lang = 'es'  # Lenguaje
+request.session_id = "_HOlaMundo_"
+
+
+def APIAI_rec(texto):
+    request = ai.text_request()
+    request.lang = 'es'  # Lenguaje
+    request.session_id = "_HOlaMundo_"
+    request.query = texto
+    response = request.getresponse()
+    res = response.read()
+    res = json.loads(res)
+    return {'action':res['result']['action'],'message':res['result']['fulfillment']['messages'][0]['speech']}
+
+Running = True
+Speach = False
+#audio from mic
+r = sr.Recognizer()
+with sr.Microphone() as source:
+    r.adjust_for_ambient_noise(source)
+while Running:
+    print()
+    if Speach:
+        #r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY")
+        with sr.Microphone() as source:
+            print("Diga algo: ")
+            audio = r.listen(source)
+        try:
+            frase = r.recognize_google(audio)
+            print("Usted dijo: " + frase)
+            res = APIAI_rec(frase)
+            arduino.arduAction(res['action'])
+            print(res['message'])
+            tts = gTTS(text=res['message'], lang='es')
+            tts.save("say.mp3")
+            playsound("./say.mp3")
+            print()
+        except:
+            print("No se le entiende nada")
+    else:
+        frase = input("Diga algo: ")
+        res = APIAI_rec(frase)
+        arduino.arduAction(res['action'])
+        print(res['message'])
+        tts = gTTS(text=res['message'], lang='es')
+        tts.save("say.mp3")
+        playsound("./say.mp3")
+        print()
+    inp = input("Preciona una tecla para continuar, S para salir>> ")
+    if inp.lower() == "s":
+        Running = False
---
+++ arduino.py
@@ -0,0 +1,23 @@
+from pyfirmata import Arduino, util
+
+def arduAction(action):
+
+    LED_PIN = 8
+    ALARM_PIN =9
+    BOARD = '/dev/tty.usbserial-A6008rIF'
+    try:
+        board = Arduino(BOARD)
+        if (action == 'luz-on'):
+            print("Encendiendo luz")
+            board.digital[LED_PIN].write(1)
+        if (action == 'luz-off'):
+            print("Apagando luz")
+            board.digital[LED_PIN].write(0)
+        if (action == 'alarm-on'):
+            print("Encendiendo alarma")
+            board.digital[ALARM_PIN].write(1)
+        if (action == 'alarm-off'):
+            print("Apagando alarma")
+            board.digital[ALARM_PIN].write(0)
+    except:
+        print("Error con aruduino")
